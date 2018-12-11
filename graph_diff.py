f6ccaa32a151338532b197b7d020de34083233f4
hewgreen <hewgreen@ebi.ac.uk>
2018-12-11 09:34:20 +0000

    edits

--- graph_diff.py
+++ graph_diff.py
@@ -4,6 +4,7 @@ import networkx as nx
 import matplotlib.pyplot as plt
 
 def load_graph(data):
+	# G=nx.DiGraph()
 	G=nx.Graph()
 	links = data['links']
 	node_names ={}
@@ -24,27 +25,51 @@ def load_graph(data):
 			G.add_edge(process_uuid,protocol_id)
 		node_names[process_uuid] = 'process'
 
-	# nx.set_node_attributes(G, node_names, 'entity_type')
+	nx.set_node_attributes(G, node_names, 'entity_type')
 
 	return G, node_names
 
 def plot_graph(G, node_names):
-	color_map = ['blue', 'green', 'red']
-	nx.draw(G, with_labels = True, labels = node_names, node_color = color_map)
+
+	node_color = []
+	for node in G.nodes(data=True):
+
+		if 'biomaterial' in node[1]['entity_type']:
+			node_color.append('darkorange')
+		elif 'file' in node[1]['entity_type']:
+			node_color.append('lightskyblue')
+		elif 'process' in node[1]['entity_type']:
+			node_color.append('mistyrose')
+		else:
+			node_color.append('olive')
+
+	nx.draw(G, with_labels=True, labels=node_names, node_color=node_color, node_size=800, font_size=8)
 	plt.show()
 
 def graph_stats(G):
-	print(G.number_of_edges())
+	# total_edges = G.number_of_edges()
+	max_depth= nx.dag_longest_path_length(G)
+	print('Max depth is {}'.format(max_depth))
+
+
+	# no_of_nodes=
+	# no_biomaterials=
+	# no_files=
+	# no_processes=
+
 
 if __name__ == '__main__':
 	
 	# Get bundle
 
-	with open('696008bb-10d0-4e5a-a7ff-ae0c84e1602c/links.json') as f:
-	# with open('48a574c8-b210-4233-86d9-43981d59fd4b/links.json') as f:
+	with open('9e385fa3-4af5-4847-8576-2d5fd1549da4/links.json') as f:
+
 		data = json.load(f)
 		graph = load_graph(data)
-		plot_graph(graph[0], graph[1])
+		G = graph[0]
+		node_names = graph[1]
+		plot_graph(G, node_names)
+		graph_stats(G)
 
 
 
