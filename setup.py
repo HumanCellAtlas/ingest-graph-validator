44dcd76d0049744e0e137a9e5976385dae06dd1f
Javier Ferrer <jferrer@ebi.ac.uk>
2019-11-29 16:27:16 +0000

    removed old readmes

--- setup.py
+++ setup.py
@@ -5,12 +5,6 @@
 
 from setuptools import setup, find_packages
 
-with open('README.rst') as readme_file:
-    readme = readme_file.read()
-
-with open('HISTORY.rst') as history_file:
-    history = history_file.read()
-
 requirements = []
 
 setup_requirements = ['pytest-runner', ]
@@ -42,7 +36,7 @@ setup(
     },
     install_requires=requirements,
     license="MIT license",
-    long_description=readme + '\n\n' + history,
+    long_description='placeholder',
     include_package_data=True,
     keywords='ingest_graph_validator',
     name='ingest_graph_validator',
