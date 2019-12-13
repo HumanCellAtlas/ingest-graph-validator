406182c60c5b6cdb2504ae5213fed10f820ff64a
Javier Ferrer <jferrer@ebi.ac.uk>
2019-12-13 16:09:13 +0000

    stringify lists

--- ingest_graph_validator/hydrators/common.py
+++ ingest_graph_validator/hydrators/common.py
@@ -7,8 +7,11 @@ def flatten(d, parent_key=""):
     for key, value in d.items():
         new_key = parent_key + "." + key if parent_key else key
 
-        if isinstance(value, collections.MutableMapping):
+        if isinstance(value, dict):
             items.extend(flatten(value, new_key).items())
+        # Convert inner lists to strings.
+        elif isinstance(value, list):
+            items.append((new_key, str(value)))
         else:
             items.append((new_key, value))
 
