a1b7d2a21fdf8294b708ff491108c5732b3718d5
Javier Ferrer <jferrer@ebi.ac.uk>
2020-01-07 15:34:11 +0000

    docstrings

--- ingest_graph_validator/data_store.py
+++ ingest_graph_validator/data_store.py
@@ -1,6 +1,12 @@
 class DataStore(object):
+    """Data store class that will be passed to the subsequent commands in the CLI."""
 
     def __init__(self):
         self.name = "empty"
-        self.entity_map = {}
+        """The name of the current graph imported into the database."""
+
         self.backend = None
+        """Neo4j docker container instance."""
+
+        self.graph = None
+        """py4neo Graph object."""
