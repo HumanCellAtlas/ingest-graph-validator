2d3b156ac7497f294ee14d302561a300b8e469cf
Javier Ferrer <jferrer@ebi.ac.uk>
2020-01-10 16:09:47 +0000

    removed interactive action

--- ingest_graph_validator/actions/action_definitions.py
+++ ingest_graph_validator/actions/action_definitions.py
@@ -4,23 +4,11 @@
 
 import click
 
-from .interactive_action import InteractiveAction
 from .test_action import TestAction
-from ..config import Config
 
 
 def get_actions():
-    return [interactive, test]
-
-
-@click.command()
-@click.option("-w", "--web-port", type=click.INT, help="Neo4j web frontend port.",
-              default=Config['NEO4J_FRONTEND_PORT'], show_default=True)
-@click.pass_context
-def interactive(ctx, web_port):
-    """Launches the Neo4j web frontend."""
-
-    InteractiveAction(web_port).run()
+    return [test]
 
 
 @click.command()
