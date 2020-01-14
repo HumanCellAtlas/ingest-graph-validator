81f43dc7383b98e08b73904e9ad5485926d3dc0b
Javier Ferrer <jferrer@ebi.ac.uk>
2020-01-14 16:27:58 +0000

    quotes

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@
 
 """The setup script."""
 
+import glob
 from os import path
 from setuptools import setup, find_packages
 
@@ -68,47 +69,46 @@ requirements = [
     "XlsxWriter==1.2.6",
 ]
 
-setup_requirements = ['pytest-runner', ]
+setup_requirements = ["pytest-runner", ]
 
-test_requirements = ['pytest>=3', ]
+test_requirements = ["pytest>=3", ]
 
-
+# Description from readme.
 this_directory = path.abspath(path.dirname(__file__))
-with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+with open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
     long_description = f.read()
-
 
 setup(
     author="Javier Ferrer Gómez",
-    author_email='jferrer@ebi.ac.uk',
-    python_requires='>=3.6',
+    author_email="jferrer@ebi.ac.uk",
+    python_requires=">=3.6",
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Natural Language :: English',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
     ],
     description="HCA Ingest Service neo4j graph validator package",
     entry_points={
         'console_scripts': [
-            'ingest-graph-validator=ingest_graph_validator.ingest_graph_validator:entry_point',
+            "ingest-graph-validator=ingest_graph_validator.ingest_graph_validator:entry_point",
         ],
     },
     install_requires=requirements,
     license="MIT license",
     long_description=long_description,
-    long_description_content_type='text/markdown',
+    long_description_content_type="text/markdown",
     include_package_data=True,
-    keywords='ingest_graph_validator',
-    name='ingest-graph-validator',
-    packages=find_packages(include=['ingest_graph_validator', 'ingest_graph_validator.*']),
+    keywords="ingest-graph-validator",
+    name="ingest-graph-validator",
+    packages=find_packages(include=["ingest_graph_validator", "ingest_graph_validator.*"]),
     setup_requires=setup_requirements,
-    test_suite='tests',
+    test_suite="tests",
     tests_require=test_requirements,
-    url='https://github.com/HumanCellAtlas/ingest-graph-validator',
-    version='0.2.0',
+    url="https://github.com/HumanCellAtlas/ingest-graph-validator",
+    version="0.2.0",
     zip_safe=False,
 )
