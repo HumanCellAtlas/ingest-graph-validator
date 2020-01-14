613506a8a73c091c4083459c45b92776dde713be
Javier Ferrer <jferrer@ebi.ac.uk>
2020-01-14 14:03:32 +0000

    Bump version: 0.1.4 → 0.1.5

--- setup.py
+++ setup.py
@@ -109,6 +109,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/HumanCellAtlas/ingest-graph-validator',
-    version='0.1.4',
+    version='0.1.5',
     zip_safe=False,
 )
