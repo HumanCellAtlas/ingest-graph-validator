dcb9c4707eb52bb83b2c380d5f9eb99d98572950
Javier Ferrer <jferrer@ebi.ac.uk>
2020-01-14 13:27:05 +0000

    Bump version: 0.1.1 → 0.1.2

--- setup.py
+++ setup.py
@@ -101,6 +101,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/javfg/ingest_graph_validator',
-    version='0.1.1',
+    version='0.1.2',
     zip_safe=False,
 )
